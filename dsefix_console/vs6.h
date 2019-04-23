bcf7d2e02ea2b3c66cffecccf00c5cd80cfc4ff7
pyj <pyj@protonmail.com>
2019-04-23 16:19:31 +0900

    little

--- dsefix_console/vs6.h
+++ dsefix_console/vs6.h
@@ -15,24 +15,25 @@ VC++ Directory - Library directory
 x64
 C:\Program Files\Microsoft Platform SDK for Windows Server 2003 R2\Lib\AMD64;
 C:\Program Files\Microsoft Platform SDK for Windows Server 2003 R2\Lib\AMD64\atlmfc;
-C:\Program Files\Microsoft Platform SDK for Windows Server 2003 R2\Lib
 x86
 C:\Program Files\Microsoft Platform SDK for Windows Server 2003 R2\Lib;
 C:\Program Files\Microsoft Platform SDK for Windows Server 2003 R2\Lib\MFC
 */
 
+//Maybe, There's no "msvcrtd.dll". So use "msvcrt.dll".
+//when using static library, try-catch does not work.
+
+//C++, Runtime Library, /MD (Multi Thread DLL)
 //C++, Code Generation, /GS-
 //C++, Code Generation, Basic Runtime Check, default
 //C++, Optimization, remove /GL
 //Linker, /SAFESEH:NO (x86)
 
-//for C++ exception handling
-//C++, Runtime Library, /MD (Multi Thread DLL)
-//Maybe, There's no "msvcrtd.dll". So use "msvcrt.dll".
-//when using static library, try-catch does not work.
-
 #ifdef _WIN64
-#pragma comment(lib, "vs6port64.lib")
+#pragma comment(lib, "vs6port.lib")
 #else
-#pragma comment(lib, "vs6port86.lib")
+void terminate();
+__declspec(naked) void __std_terminate() { __asm jmp terminate }
+extern "C" void __CxxFrameHandler();
+extern "C" __declspec(naked) void __CxxFrameHandler3() { __asm jmp __CxxFrameHandler }
 #endif
